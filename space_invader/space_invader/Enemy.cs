ee9863abd42c923214b66e3c5ddbe08246db87c6
hindrekhan <33310195+hindrekhan@users.noreply.github.com>
2018-06-04 11:16:11 +0300

    add enemy

---
+++ space_invader/space_invader/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace space_invader
+{
+    class Enemy : Entity
+    {
+
+        Enemy()
+        {
+
+        }
+
+        void LoadEnemies()
+        {
+
+        }
+
+        void UpdateMovement()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+
+        }
+    }
+}
