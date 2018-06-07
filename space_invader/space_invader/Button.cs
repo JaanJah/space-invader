f2de3e264d820f5e0b1efed526dfde1c5a42335e
hindrekhan <33310195+hindrekhan@users.noreply.github.com>
2018-06-07 13:19:06 +0300

    button can be pressed only once

--- space_invader/space_invader/Button.cs
+++ space_invader/space_invader/Button.cs
@@ -12,7 +12,8 @@ namespace space_invader
 {
     class Button : Entity
     {
-        
+        bool pressed = false;
+
         public Button (float x, float y) : base(x, y)
         {
             Image buttonOutline = new Image("../../../Assets/buttonOutline.png");
@@ -25,12 +26,18 @@ namespace space_invader
 
             if (Input.MouseButtonPressed(MouseButton.Left))
             {
-                if (Util.InRect(Scene.MouseX, Scene.MouseY, X,Y, 80, 30))
+                if (!pressed)
                 {
-                    var inputText = Scene.GetEntity<TextBox>().inputString;
-                    MainScene scene = Program.game.GetScene<MainScene>();
-                    var a = Program.game.Scene;
-                    Leaderboard.AddScore(inputText, Program.curScoreTxt);
+                    if (Util.InRect(Scene.MouseX, Scene.MouseY, X, Y, 80, 30))
+                    {
+                        var inputText = Scene.GetEntity<TextBox>().inputString;
+                        MainScene scene = Program.game.GetScene<MainScene>();
+                        var a = Program.game.Scene;
+                        Leaderboard.AddScore(inputText, Program.curScoreTxt);
+
+
+                        pressed = true;
+                    }
                 }
             }
         }
