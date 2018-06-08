2e02ddbb7f24fbfb59755d15a4eb9dd851974405
JaanJahimees <jaanjahilo@outlook.com>
2018-06-08 08:55:46 +0300

    removed unnecessary usings

--- space_invader/space_invader/ReadXML.cs
+++ space_invader/space_invader/ReadXML.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 using Otter;
 
 namespace space_invader
